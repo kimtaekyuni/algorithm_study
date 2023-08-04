a84b3ec80819c129c4b754dc733d3fdc5069dcad
{taekyun} <tgkim2003@gmail.com>
2023-08-04 10:25:56 +0900

    자물쇠 문제

--- programmas_lock_and_key.py
+++ programmas_lock_and_key.py
@@ -1,5 +1,6 @@
 # 자물쇠와 열쇠
 
+'''원래 풀이
 # NxN 2차원 리스트 d도 회전
 # 회전 각도 d => 1: 90도, 2: 180도, 3: 270도
 def rotate(array, d):
@@ -24,9 +25,10 @@ def rotate(array, d):
                 result[r][c] = array[r][c]
 
     return result
+#회전 값에 따라 다시 배열 완료 
 
 
-# 자물쇠 중간 NxN 부분이 모두 1인지 확인
+# 자물쇠 중간 NxN 부분이 모두 1인지 확인 
 def check(new_lock):
     n = len(new_lock) // 3
     for i in range(n, n * 2):
@@ -62,5 +64,45 @@ def solution(key, lock):
                 for x in range(m):
                     for y in range(m):
                         new_lock[i + x][j + y] -= r_key[x][y]
+
+    return False '''
+
+#영상 보고 다시 풀이 
+def match(arr, key, rot, r, c):
+    n = len(key)
+    for i in range(n):
+        for j in range(n):
+            if rot == 0:
+                arr[r + i][c + j] += key[i][j]    
+            elif rot == 1:
+                arr[r + i][c + j] += key[n-1 -j][i]
+            elif rot == 2:
+                arr[r + i][c + j] += key[n-1 -i][n-1 -j]
+            else: 
+                arr[r + i][c + j] += key[j][n-1 -i]
+
+def check(arr, offset, n):
+    for i in range(n):
+        for j in range(n):
+            if arr[offset + i][offset + j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    offset = len(key) - 1
+
+    #모든 경우의 수를 만들어내기 위해서
+    for r in range(offset + len(lock)) # r은 행의 좌표
+        for c in range(offset + len(lock)): #c는 행의 좌표
+            for rot in range(4):
+                arr = [[ 0 for _ in range(58)] for _ in range(58)] #arr은 배열의 이름
+                for i in range(len(lock)):
+                    for j in range(len(lock)):
+                        arr[offset + i][offset + j] = lock[i[j]]
+                
+                match(arr, key, rot, r, c)
+                if check(arr, offset, len(lock)):
+                    return True
 
     return False
